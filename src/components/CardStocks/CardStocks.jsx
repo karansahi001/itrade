f4e8663600660c00648320cfd08bb3a795aca55e
karansahi001 <iamkaransahi@gmail.com>
2023-06-21 16:49:39 -0400

    trending pages setup

---
+++ src/components/CardStocks/CardStocks.jsx
@@ -0,0 +1,11 @@
+import { Paper } from '@mui/material';
+
+const CardStocks = () => {
+  return (
+    <Paper elevation={3}>
+      <h1>Hello</h1>
+    </Paper>
+  )
+}
+
+export default CardStocks
--- src/pages/TrendingStocks/TrendingStocks.jsx
+++ src/pages/TrendingStocks/TrendingStocks.jsx
@@ -1,14 +1,47 @@
 import { useDispatch } from 'react-redux';
 import { updateNav } from '../../redux/navSlice';
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import CardStocks from '../../components/CardStocks/CardStocks'
+import { Grid, Paper, Typography } from '@mui/material';
+import './TrendingStocks.scss'
+
+
+// import { auth } from '../../config/firebase';
 
 const TrendingStocks = () => {
+  const apiUrl = process.env.REACT_APP_API_URL;
+  const [data, setData] = useState([]);
   const dispatch = useDispatch();
-
   dispatch(updateNav("trending"));
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${apiUrl}/stocks`)
+        setData(response.data.tickers)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchData();
+  }, [apiUrl])
+
+
   return (
-    <div>
-      <h1>Trending</h1>
-    </div>
+    <main className="trending">
+      <Paper elevation={2} className="trending-stocks">
+        <Typography variant="h4" sx={{color: "primary.main"}}>Trending Stocks:</Typography>
+        <section>
+          
+        </section>
+      </Paper>
+      <Paper elevation={2} className="trending-watchlist">
+        <Typography variant="h4" sx={{color: "primary.main"}}>Watchlist:</Typography>
+      </Paper>
+      
+        {/* <CardStocks /> */}
+    </main>
   )
 }
 
